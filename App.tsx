e760e1a48fe309f1a37a30296397469543f7e58f
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-04 16:00:51 -0300

    Integrate ThemeProvider and define theme colors and fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import { Groups } from "@screens/Groups";
 
+import theme from "./src/theme";
+
+import { ThemeProvider } from "styled-components";
+
 export default function App() {
-  return <Groups></Groups>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups></Groups>
+    </ThemeProvider>
+  );
 }
