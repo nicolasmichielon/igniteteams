2e2a63d8b64b21b0394cfe950b20685dd6759ca6
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-07 13:13:55 -0300

    Add Header, Highlight, and GroupCard components with styles; declare PNG module

---
+++ src/components/GroupCard/styles.ts
@@ -0,0 +1,32 @@
+import { TouchableOpacity } from "react-native";
+import theme from "src/theme";
+import styled from "styled-components/native";
+import UsersThree from "phosphor-react-native/src/icons/UsersThree";
+
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  height: 90px;
+
+  background-color: ${theme.COLORS.GRAY_500};
+  border-radius: 6px;
+
+  flex-direction: row;
+  align-items: center;
+
+  padding: 24px;
+  margin-bottom: 12px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${theme.FONT_SIZE.MD}px;
+  color: ${theme.COLORS.GRAY_200};
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+`;
+
+export const Icon = styled(UsersThree).attrs({
+  size: 32,
+  color: theme.COLORS.GREEN_700,
+  weight: "fill",
+})`
+  margin-right: 20px;
+`;
