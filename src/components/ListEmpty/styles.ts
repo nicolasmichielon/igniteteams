d607b3be5122672557d8b9288fbe457fb99388d2
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-07 14:45:55 -0300

    Add ListEmpty component and integrate it into Groups screen for empty state handling

---
+++ src/components/ListEmpty/styles.ts
@@ -0,0 +1,16 @@
+import theme from "src/theme";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Message = styled.Text`
+  text-align: center;
+
+  font-size: ${theme.FONT_SIZE.SM}px;
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  color: ${theme.COLORS.GRAY_300};
+`;
