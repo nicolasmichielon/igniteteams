ea33875c38cc02a80cee1efea33badea3081e3d5
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-08 14:05:59 -0300

    Add PlayerCard component and integrate it into Players screen

---
+++ src/components/PlayerCard/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+import { MaterialIcons } from "@expo/vector-icons";
+import theme from "src/theme";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 56px;
+
+  background-color: ${theme.COLORS.GRAY_500};
+  border-radius: 6px;
+
+  flex-direction: row;
+  align-items: center;
+
+  margin-bottom: 16px;
+`;
+
+export const Name = styled.Text`
+  flex: 1;
+
+  font-size: ${theme.FONT_SIZE.MD}px;
+  color: ${theme.COLORS.GRAY_200};
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+`;
+
+export const Icon = styled(MaterialIcons).attrs({
+  size: 24,
+  color: theme.COLORS.GRAY_200,
+})`
+  margin-left: 16px;
+  margin-right: 4px;
+`;
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,6 +6,9 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 import { useState } from "react";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
@@ -41,6 +44,24 @@ export function Players() {
         />
         <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
